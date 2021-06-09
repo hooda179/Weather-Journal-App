a625e07361d59bb5b7406372b383653b0235407d
mahmoud <hooda.179@gmail.com>
2021-06-09 18:30:14 +0200

    before seperatee code with functions

--- website/app.js
+++ website/app.js
@@ -10,61 +10,37 @@ const apiKey = "d82658548f63a4e3a221948a732608da";
 let d = new Date();
 let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 
-// add a click event to generate weather state
 btn.addEventListener("click",async () => {
     const value = zipCode.value;
     const url = `http://api.openweathermap.org/data/2.5/weather?zip=${value}&appid=${apiKey}&units=metric`;
+    const feelings = document.getElementById("feelings").value;
 
-    respondData(url).then((data) => {
-        postData(data);
-    }).then(() =>{
-        updateUI();
-    }).catch((error)=>{
-        console.log(error);
-    })
-        
-    
-})
-
-// declaring the functions 
-async function respondData(url){
     const respond = await fetch(url);
     const data = await respond.json();
     try {
-        return data;
-    }
-    catch(error) {
-        console.log("error: " , data.message);
-    }
-}
-
-async function postData(data){
-    const temp = data.main.temp;
-    const feelings = document.getElementById("feelings").value;
-    await fetch("/addData",{
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            date: newDate,
-            temp: temp,
-            feeling: feelings
+        const temp = data.main.temp;
+        
+        await fetch("/addData",{
+            method: "POST",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                date: newDate,
+                temp: temp,
+                feeling: feelings
+            })
         })
-    })
-}
 
-async function updateUI(){
-    const res = await fetch("/all");
-    const weatherData = await res.json();
-    try {
+        const res = await fetch("/all");
+        const weatherData = await res.json();
+
         date.textContent = weatherData.date;
         temperature.textContent = weatherData.temp;
         content.textContent = weatherData.feeling;
     }
     catch(error){
-        console.log("error: ", error);
+        alert(data.message);
     }
-    
-}
+})
