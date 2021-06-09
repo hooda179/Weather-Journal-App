6a5c3436539627d8c9a392b72717a5680962c2e5
mahmoud <hooda.179@gmail.com>
2021-06-09 18:56:06 +0200

    adding alert asking user for zip code

--- website/app.js
+++ website/app.js
@@ -13,34 +13,40 @@ let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 btn.addEventListener("click",async () => {
     const value = zipCode.value;
     const url = `http://api.openweathermap.org/data/2.5/weather?zip=${value}&appid=${apiKey}&units=metric`;
-    const feelings = document.getElementById("feelings").value;
 
-    const respond = await fetch(url);
-    const data = await respond.json();
-    try {
-        const temp = data.main.temp;
-        
-        await fetch("/addData",{
-            method: "POST",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                date: newDate,
-                temp: temp,
-                feeling: feelings
+    if(!value){
+        alert("Please, enter zip code!");
+    }else {
+        // recieve data from openWeatherMap api 
+        const respond = await fetch(url);
+        const data = await respond.json();
+        try {
+            // add data to our end point 
+            const feelings = document.getElementById("feelings").value;
+            const temp = data.main.temp;
+            await fetch("/addData",{
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    date: newDate,
+                    temp: temp,
+                    feeling: feelings
+                })
             })
-        })
 
-        const res = await fetch("/all");
-        const weatherData = await res.json();
+            // recieve data from end point 
+            const res = await fetch("/all");
+            const weatherData = await res.json();
 
-        date.textContent = weatherData.date;
-        temperature.textContent = weatherData.temp;
-        content.textContent = weatherData.feeling;
-    }
-    catch(error){
-        alert(data.message);
+            date.textContent = weatherData.date;
+            temperature.textContent = weatherData.temp;
+            content.textContent = weatherData.feeling;
+        }
+        catch(error){
+            alert(data.message);
+        }    
     }
 })
